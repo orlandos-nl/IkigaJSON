b579e7e4f1f58c45dc11b2aeb49a4ace88b550f3
Joannis orlandos <joannis@orlandos.nl>
2021-07-09 10:20:24 +0200

    Remove more Linuxmain

--- Tests/IkigaJSONTests/XCTestManifests.swift
+++ Tests/IkigaJSONTests/XCTestManifests.swift
@@ -1,10 +1,1 @@
 import XCTest
-
-#if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(CSVTests.allTests),
-        testCase(JSONTests.allTests),
-    ]
-}
-#endif
