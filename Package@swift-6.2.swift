e2e1d5ec0da970220dc1282a4399d7920f77d173
Joannis Orlandos <joannis@orlandos.nl>
2025-05-30 10:56:18 +0200

    Support for Swift 6.2 spans

--- Package@swift-6.2.swift
+++ Package@swift-6.2.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,6 +16,10 @@ let package = Package(
             targets: ["IkigaJSON"]
         )
     ],
+    traits: [
+        .trait(name: "Spans"),
+        .trait(name: "SourcePositions"),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
@@ -24,7 +28,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "_JSONCore"
+            name: "_JSONCore",
+            swiftSettings: [
+                .enableExperimentalFeature("LifetimeDependence")
+            ]
         ),
         .target(
             name: "_NIOJSON",
